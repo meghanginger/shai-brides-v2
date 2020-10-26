bea1dbc1041626a32074979050023f7c2b4ebb20
Meghan Avery <Meghan@Meghans-Mega.local>
2020-10-26 23:42:36 +0000

    fix gitignore

--- public/styles-009b879b767b30d705ae.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{184:function(n,w,o){}}]);
-//# sourceMappingURL=styles-009b879b767b30d705ae.js.map
--- public/styles-2beda48ca03daa49990d.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{"1/6+":function(n,w,o){}}]);
-//# sourceMappingURL=styles-2beda48ca03daa49990d.js.map
